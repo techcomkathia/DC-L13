733405c1bf4e899f10796967acb41c3cdb0455d1
techcomkathia <techcomk@gmail.com>
2026-05-05 20:30:34 -0300

    introdução ao react

---
+++ REACT/Aula28/src/componentes/MeuPrimeiroComponente.jsx
@@ -0,0 +1,14 @@
+function MeuPrimeiroComponente() {
+    return (
+        
+        <div>
+            <h1>Primeiro Componente</h1>
+
+            <p>Texto dentro de um parágrafo no componente Meu Primeiro Componente</p>
+        </div>
+        
+    );
+}
+
+//exportação para possibilidade de uso em outro arquivo
+export default MeuPrimeiroComponente
