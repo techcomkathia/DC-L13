2a2b29f54770fcfb71ad82a3f310d8d382ad4200
techcomkathia <techcomk@gmail.com>
2026-05-12 19:17:13 -0300

    bibliotecas de estilo e componentização

---
+++ REACT/Aula30/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css' //folha de estilo do bootstrap
+import Botoes from './componentes/Botoes'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Aula 30</h1>
+      <Botoes/>
+    </>
+  )
+}
+
+export default App
