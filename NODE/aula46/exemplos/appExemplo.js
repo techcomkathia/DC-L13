53a65febe06918573ac91f8183cbd1a9bfbf54a6
techcomkathia <techcomk@gmail.com>
2026-07-14 21:12:29 -0300

    atribuição do controlador a rota

--- NODE/aula46/exemplos/appExemplo.js
+++ NODE/aula46/exemplos/appExemplo.js
@@ -1,2 +1,18 @@
 //iniciar com a instalação do express sequelize, mysql2 e nodemon
 
+// a ordem da camada mais próxima do banco de dados para a camada mais próxima das rotas do express
+// model -> services-> controller -> rotas -> (consumo do usuario/ camada de visualizacao) -> view
+
+const express = require('express')
+const CategoriasController = require('./3.Controladores/CategoriasController')
+
+const app = express()
+//utilizar o middleware para habilitar o uso do JSON no corpo da requisição
+app.use(express.json())
+
+app.get('/', (req, res) => res.json({mensagem: 'Olá, seja bem-vindo a nossa API!'}))
+app.get('/categorias', (req, res) => CategoriasController.getCategorias(req, res))
+
+app.listen(3000, ()=>{
+    console.log('servidor disponível em http://localhost:3000/categorias')
+})
