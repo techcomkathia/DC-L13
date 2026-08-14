31665fe933def0d3a7ea01dcc760c0f8b7f1340d
techcomkathia <techcomk@gmail.com>
2026-08-13 21:56:31 -0300

    feat: adiciona correção da atividade3

--- REVISAO/aula55/atividade3.js
+++ REVISAO/aula55/atividade3.js
@@ -226,15 +226,111 @@ class ContaSalario extends ContaBancaria{
 //mostrarInformacoes (deve incluir a quantidade de contas ativas)
 //getContas
 
+class AgenciaBancaria{
+    #contas = []
+    #gerente
+    #telefone
+    #numeroAgencia
+    constructor(endereco, gerente, telefone, numeroAgencia){
+        this.endereco = endereco
+        this.#gerente = gerente
+        this.#telefone = telefone
+        this.#numeroAgencia = numeroAgencia
+        this.#contas = []
+    }
+
+    //metodos de acesso e atribuição
+    setGerente(gerente){
+        this.#gerente = gerente
+    }
+    getGerente(){
+        return this.#gerente
+    }
+    setTelefone(telefone){
+        this.#telefone = telefone
+    }
+    getTelefone(){
+        return this.#telefone
+    }
+
+    getContas(){
+        return this.#contas
+    }
+
+    getNumeroAgencia(){
+        return this.#numeroAgencia
+    }
+
+    setNumeroAgencia(numeroAgencia){
+        this.#numeroAgencia = numeroAgencia
+    }
+
+    adicionarConta(conta){
+        if(conta instanceof ContaBancaria || conta instanceof ContaPoupanca || conta instanceof ContaSalario || conta instanceof ContaCorrente){
+            this.#contas.push(conta)
+        }else{
+            throw new Error('A conta precisa ser da classe ContaBancaria')
+        }
+    }
+
+    removerConta(numero){
+        const conta = this.buscarContaPorNumero(numero)
+        if(!conta){
+            throw new Error('Conta nao encontrada')
+        }
+        this.#contas.splice(this.#contas.indexOf(conta), 1) //remove a conta pelo indice
+    }
+
+    buscarContaPorNumero(numero){
+        const conta = this.#contas.find(conta => conta.getNumero() == numero)
+        if(!conta){
+            throw new Error('Conta nao encontrada')
+        }
+        return conta
+    }    
+
+    buscarContaPorTitular(titular){
+        const conta = this.#contas.find(conta => conta.getTitular() == titular)
+        if(!conta){
+            throw new Error('Conta nao encontrada')
+        }
+        return conta
+    }
+
+    mostrarInformacoes(){
+        return(`Agencia: ${this.#numeroAgencia}, Gerente: ${this.#gerente}, Telefone: ${this.#telefone}, Quantidade de contas ativas: ${this.#contas.length}`)
+    }
+
+}
 
 //Classes filhas: 
 //AgenciaFisica
+
 //Atributo:
 //endereco
 //getEndereco
 //setEndereco
 //mostrarInformacoes(polimorfismo)
+class AgenciaFisica extends AgenciaBancaria{
+    #endereco
+    constructor(endereco, gerente, telefone, numeroAgencia){
+        super(gerente, telefone, numeroAgencia)
+        this.#endereco = endereco
+    }
 
+    getEndereco(){
+        return this.#endereco
+    }
+
+    setEndereco(endereco){
+        this.#endereco = endereco
+    }
+
+    //polimorfismo
+    mostrarInformacoes(){
+        return(`Agencia: ${this.getNumeroAgencia()}, Gerente: ${this.getGerente()}, Telefone: ${this.getTelefone()}, Quantidade de contas ativas: ${this.getContas().length}, Endereco: ${this.#endereco}`)
+    }
+}
 
 //AgenciaVirtual
 //Atributo:
@@ -247,8 +343,69 @@ class ContaSalario extends ContaBancaria{
 //metodos:
 //mostrarInformacoes(polimorfismo)
 
+class AgenciaVirtual extends AgenciaBancaria{
+    #site
+    #linkAplicativo
+    constructor(site, linkAplicativo, gerente, telefone, numeroAgencia){
+        super(gerente, telefone, numeroAgencia)
+        this.#site = site
+        this.#linkAplicativo = linkAplicativo
+    }
+
+    getSite(){
+        return this.#site
+    }
+
+    getLinkAplicativo(){
+        return this.#linkAplicativo
+    }
+
+    setSite(site){
+        this.#site = site
+    }
+
+    setLinkAplicativo(linkAplicativo){
+        this.#linkAplicativo = linkAplicativo
+    }
+
+    mostrarInformacoes(){
+        return(`Agencia: ${this.getNumeroAgencia()}, Gerente: ${this.getGerente()}, Telefone: ${this.getTelefone()}, Quantidade de contas ativas: ${this.getContas().length}, Site: ${this.#site}, Link do aplicativo: ${this.#linkAplicativo}`)
+    }
+}
 
 //Instacie uma agenciaVirtual com 5 contas bancarias
+const conta1 = new ContaPoupanca('João Silva', 123, 1000)
+const conta2 = new ContaCorrente('Maria Santos', 456, 5000, 5000, 0)
+const conta3 = new ContaSalario('Pedro Oliveira', 789, 2000, 1000)
+const conta4 = new ContaBancaria('Ana Costa', 321, 3000)
+const conta5 = new ContaBancaria('Luiz Santos', 654, 4000)
+const agenciaVirtual = new AgenciaVirtual('www.agenciavirtual.com.br', 'www.agenciavirtual.com.br', 'Cleitinho Lima', '1234-5678', '0001')
 //Instacie uma agenciaFisica com 3 contas bancarias
+const conta6 = new ContaPoupanca('João Silva', 123, 1000)
+const conta7 = new ContaCorrente('Maria Santos', 456, 5000, 5000, 0)
+const conta8 = new ContaSalario('Pedro Oliveira', 789, 2000, 1000)
+const agenciaFisica = new AgenciaFisica('Rua 123, 456', 'Cleitinho Lima', '1234-5678', '0001')
+
+//adicionarContas
+agenciaVirtual.adicionarConta(conta1)
+agenciaVirtual.adicionarConta(conta2)
+agenciaVirtual.adicionarConta(conta3)
+agenciaVirtual.adicionarConta(conta4)
+agenciaVirtual.adicionarConta(conta5)
+
+agenciaFisica.adicionarConta(conta6)
+agenciaFisica.adicionarConta(conta7)
+agenciaFisica.adicionarConta(conta8)
+
+
+//execute o método mostrarInformacoes para ambas as agencias
+console.log(agenciaVirtual.mostrarInformacoes())
+console.log(agenciaFisica.mostrarInformacoes())
+
+agenciaVirtual.getContas().forEach(conta => console.log(conta.mostrarDetalhes()))
+
+agenciaFisica.getContas().forEach(conta => console.log(conta.mostrarDetalhes()))
+
+
 //execute o método mostrarInformacoes para ambas as agencias
 //execute o método mostrarDetalhes para todas as contas de ambas as agencias
